ff71ae88a5b694f61a189cdd127614ed1fb7afb1
Joaquin Roibal <jroibal@nmt.edu>
2016-04-20 16:00:53 -0600

    Editing the Plotting to repeat less, use for loops
    Reduced repeated code, used for k in [0.5, 1, 2.5]

--- Tunneling_Stresses.py
+++ Tunneling_Stresses.py
@@ -41,126 +41,51 @@ def OverStressZone(RadStress, TanStress, ShrStress, c=800, phi=32):
     
 def main():
 
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
-    Pz = 2500 * 1.1
-    Pi = 0
-    Diameter = 10
-    k = 0.5
-    Theta = 90
+    for k in [0.5, 1, 2.5]:
+        RadStress = []
+        TanStress = []
+        ShrStress = []
+        r_list = []
+        Pz = 2500 * 1.1
+        Pi = 0
+        Diameter = 10
+        Theta = 90
     #Determine Stresses for K = 0.5 and Plot Resulting Stresses
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))        
-        r_list.append(r)
-    plt.figure(1)
-    #plt.subplot(311)
-    plt.plot(r_list, RadStress, '|-', label='Radial Stress, K= {}'.format(k))
-    plt.plot(r_list, TanStress, label='Tangential Stress, K= {}'.format(k))
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses'.format(k))
+        for r in range(5,60,1):
+            RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
+            TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
+            ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))        
+            r_list.append(r)
+        plt.figure(1)
+        plt.plot(r_list, RadStress, '|-', label='Radial Stress, K= {}'.format(k))
+        plt.plot(r_list, TanStress, label='Tangential Stress, K= {}'.format(k))
+        plt.xlabel('Feet')
+        plt.ylabel('Stress (PSI)')
+        plt.title('Tangential and Radial Stresses')
 
-    #Determine Stresses for K = 1.0 and Plot Resulting Stresses
-    k = 1
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
-        r_list.append(r)
 
-    #plt.subplot(312)
-    plt.plot(r_list, RadStress, '|-', label='Radial Stress, K= {}'.format(k))
-    plt.plot(r_list, TanStress, label='Tangential Stress, K= {}'.format(k))
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses, K = {}'.format(k))
 
-    #Determine Stresses for K = 2.5 and Plot
-    k = 2.5
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
-        r_list.append(r)
-
-    #plt.subplot(313)
-    plt.plot(r_list, RadStress, '|-', label='Radial Stress, K= {}'.format(k))
-    plt.plot(r_list, TanStress, label='Tangential Stress, K= {}'.format(k))
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses Circular Tunnel'.format(k))
-    plt.legend()
-#Graphing for Internal Pressures
-    Pi = 500
     
-    k = 0.5
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
+    #Graphing for Internal Pressure = 500 PSI
+    for k in [0.5, 1, 2.5]:
+        Pi = 500
+        RadStress = []
+        TanStress = []
+        ShrStress = []
+        r_list = []
     #Determine Stresses for K = 0.5 and Plot Resulting Stresses
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
-        r_list.append(r)
-    plt.figure(2)
+        for r in range(5,60,1):
+            RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
+            TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
+            ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
+            r_list.append(r)
+        plt.figure(2)
     #plt.subplot(311)
-    plt.plot(r_list, RadStress)
-    plt.plot(r_list, TanStress)
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses, K = {}, Internal Pressure of 500'.format(k))
-
-    #Determine Stresses for K = 1.0 and Plot Resulting Stresses
-    k = 1
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
-        r_list.append(r)
-
-    #plt.subplot(312)
-    plt.plot(r_list, RadStress)
-    plt.plot(r_list, TanStress)
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses, K = {}, Internal Pressure of 500'.format(k))
-
-    #Determine Stresses for K = 2.5 and Plot
-    k = 2.5
-    RadStress = []
-    TanStress = []
-    ShrStress = []
-    r_list = []
-    for r in range(5,60,1):
-        RadStress.append(RadialStress(Pz, Diameter, k, Theta, r, Pi))
-        TanStress.append(TangentialStress(Pz, Diameter, k, Theta, r, Pi))
-        ShrStress.append(ShearStress(Pz, Diameter, k, Theta, r))
-        r_list.append(r)
-
-    #plt.subplot(313)
-    plt.plot(r_list, RadStress)
-    plt.plot(r_list, TanStress)
-    plt.xlabel('Feet')
-    plt.ylabel('Stress (PSI)')
-    plt.title('Tangential and Radial Stresses, K = {}, Internal Pressure of 500'.format(k))    
+        plt.plot(r_list, RadStress, '|-', label='Radial Stress, K= {}'.format(k))
+        plt.plot(r_list, TanStress, label='Tangential Stress, K= {}'.format(k))
+        plt.xlabel('Feet')
+        plt.ylabel('Stress (PSI)')
+        plt.title('Tangential and Radial Stresses, Internal Pressure of 500')
 
     plt.show()
 
