b6bc94c4d78479c35e399226d4e8c65a28c87ef1
Joaquin Roibal <jroibal@nmt.edu>
2016-05-10 09:07:21 -0600

    Added PlotExample(), FactorOfSafety()

--- Pillar_Design.py
+++ Pillar_Design.py
@@ -1,13 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def PillarDesign(sigma_z, pillar_width, room_width, height, design_fos):
     """
-    The Purpose of this program is to successfully design pillar based on example Measurements.
+    The Purpose of this program is to successfully design pillar for room and
+    pillar underground mining method based on example Measurements.
     Copyright Joaquin Roibal 2016
     New Mexico Institute of Mining and Technology
-    :return:
     """
-    pillar_stress = sigma_z*((pillar_width+room_width)/pillar_width)**2
+    
+    pillar_stress = PillarStress(sigma_z, pillar_width, room_width)
     ps = PillarStrength(height,pillar_width)
-    factor_of_safety = ps/pillar_stress
+    factor_of_safety = FactorOfSafety(ps, pillar_stress)
+
     print("Original Pillar Stress: {}".format(pillar_stress))
     print("Calculated Pillar Strength: {}".format(ps))
     print("Factor of Safety of Original Design Requirements: {}".format(factor_of_safety))
@@ -16,10 +21,10 @@ def PillarDesign(sigma_z, pillar_width, room_width, height, design_fos):
         room_width += .1
         pillar_width -= .1
         ps = PillarStrength(height, pillar_width)
-        pillar_stress = sigma_z*((pillar_width+room_width)/pillar_width)**2
-        factor_of_safety = ps/pillar_stress
+        pillar_stress = PillarStress(sigma_z, pillar_width, room_width)
+        factor_of_safety = FactorOfSafety(ps, pillar_stress)
     print("Final Design Requirements for FOS = {}\nRoom Width: {} \nPillar Width: {}".format(factor_of_safety,
-                                                                                             room_width, pillar_width))
+                                                                                          room_width, pillar_width))
     print("Pillar Strength: {}".format(ps))
     print("Extraction Ratio: {}".format(ExtractionRatio(pillar_width,room_width)))
     print("Extraction Ratio: {}".format(ExtractionRatio(pillar_width,room_width)))
@@ -28,8 +33,8 @@ def PillarDesign(sigma_z, pillar_width, room_width, height, design_fos):
         room_width -= .1
         pillar_width +=.1
         ps = PillarStrength(height,pillar_width)
-        pillar_stress = sigma_z*((pillar_width+room_width)/pillar_width)**2
-        factor_of_safety = ps/pillar_stress
+        pillar_stress = PillarStress(sigma_z, pillar_width, room_width)
+        factor_of_safety = FactorOfSafety(ps, pillar_stress)
     print("Final Design Requirements for FOS = {}\nRoom Width: {} \nPillar Width: {}".format(factor_of_safety,
                                                                                              room_width, pillar_width))
     print("Pillar Stress: {}".format(pillar_stress))
@@ -39,8 +44,53 @@ def PillarDesign(sigma_z, pillar_width, room_width, height, design_fos):
 def PillarStrength(height, width):
     return 10.44*height**(-.7)*width**(0.5)
 
+def FactorOfSafety(p_strength, p_stress):
+    return p_strength/p_stress
+
 def ExtractionRatio(pillar_width,room_width):
     return ((pillar_width+room_width)**2-(pillar_width**2))/((pillar_width+room_width)**2)
+
+def PillarStress(sigma_z, pillar_width, room_width):
+    return sigma_z*((pillar_width+room_width)/pillar_width)**2
+
+def PlotExample():
+    """
+    Example Plot for varying Pillar, Room Widths which displays the
+    Varying Extraction Ratios.
+    """
+    er = []
+    wp = [x for x in range(1,15)]
+    wo = [y for y in range(1,15)]
+    for w_p in wp:
+        w_open = []
+        for w_o in wo:
+            w_open.append(ExtractionRatio(w_p,w_o))
+        er.append(w_open)
+        plt.plot(wo, w_open, label = 'Pillar Length: {}'.format(w_p))    
+    plt.xlabel('Width Opening of Tunnel')
+    plt.ylabel('Extraction Ratio')
+    plt.title('Extraction Ratio, Varying Room and Pillar Sizing')
+    print(er)
+    plt.legend(loc=4)
+    
+    plt.figure(2)
+    er2 = []
+    sigma_z = 3.375
+    height = 3
+    for w_p in wp:
+        w_open2 = []
+        for w_o in wo:
+            pstress = PillarStress(sigma_z, w_p, w_o)
+            pstrength = PillarStrength(height, w_p)
+            FOS = FactorOfSafety(pstrength, pstress)
+            w_open2.append(FOS)
+        er2.append(w_open2)
+        plt.plot(wo, w_open2, label = 'Pillar Length: {}'.format(w_p))
+    plt.xlabel('Width Opening of Tunnel')
+    plt.ylabel('Factor of Safety')
+    plt.title('Factor of Safety, Varying Room and Pillar Sizing')
+    plt.legend(loc=1)
+    plt.show()
 
 def main():
     depth = 150                             #Meters
@@ -52,6 +102,8 @@ def main():
     design_fos = [1.3, 1.6]
     for fos in design_fos:
         PillarDesign(sigma_z, pillar_width, room_width, height, fos)
+
+    PlotExample()
 
 if __name__ == "__main__":
     main()
